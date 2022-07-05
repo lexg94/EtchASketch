b3970344b039c922342d50c89e1eae4a10fa6c0d
Alexander <alex.gienko@gmail.com>
2022-07-05 14:58:27 +0300

    Added ability to change grid size with user input, done for now

--- script.js
+++ script.js
@@ -1,42 +1,80 @@
-const container = document.getElementById("container");
+const container = document.getElementById("grid-container");
+const input = document.getElementById("val");
 
 
-var rows = 8;
-var cols = 8;
+input.addEventListener("change", () => {
+    rows = input.value;
+    cols = input.value;
 
-container.innerHTML = "";
+    // test for out of bound values
+    if(rows > 100 || rows < 1) {
+        alert('Illegal value entered, please input a number between 1 and 100');
+    }
+    else {
+        changeGrid();
+    }
+});
 
+// make default 8x8 grid
 
-container.style.setProperty('--grid-rows',rows);
-container.style.setProperty('--grid-cols',cols);
 
 function makeGrid() {
 
-    for (let i = 0; i < rows*cols; i++) {
+    container.style.setProperty('--grid-rows',8);
+    container.style.setProperty('--grid-cols',8);
+
+    for (let i = 0; i < 64; i++) {
 
         const divs = document.createElement("div");
         container.appendChild(divs).className = "grid-item";
     }
+
+    changeBackground();
+}
+
+//update grid dimensions based on user input
+
+function changeGrid() {
+
+    container.innerHTML = "";
+
+    container.style.setProperty('--grid-rows',rows);
+    container.style.setProperty('--grid-cols',cols);
+
+    for (let i = 0; i < rows * cols; i++) {
+
+        const divs = document.createElement("div");
+        container.appendChild(divs).className = "grid-item";
+    }
+
+    changeBackground();
+
+}
+
+// change background color of square
+
+function changeBackground() {
+
+    const grids = document.querySelectorAll(".grid-item");
+    const reset = document.querySelector(".reset-btn");
+
+    grids.forEach((grid) => {
+
+        grid.addEventListener('mouseover', function(e) {
+
+            e.target.style.backgroundColor = "red";           
+        });
+
+        reset.addEventListener('click', () => {
+            grid.style.backgroundColor = "white";
+        });
+    });
 }
 
 makeGrid();
 
 
-const grids = document.querySelectorAll(".grid-item");
-const reset = document.querySelector(".reset-btn");
 
-
-grids.forEach((grid) => {
-
-    grid.addEventListener('mouseover', function(e) {
-
-        e.target.style.backgroundColor = "red";           
-    });
-
-    reset.addEventListener('click', () => {
-        grid.style.backgroundColor = "white";
-    });
-});
 
 
 
